31af9b4b50b2794499a5bfb6167ed2e5e8bcccce
kml-converter <ing.alflab@gmail.com>
2026-07-20 23:24:22 +0200

    Upgrade Flutter and Android plugin versions
    Updated Flutter and Android plugin versions in settings.gradle.kts.

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,17 +1,4 @@
 pluginManagement {
-    val flutterSdkPath = run {
-        val properties = java.util.Properties()
-        val file = settingsDir.resolve("local.properties")
-        if (file.exists()) {
-            file.reader(Charsets.UTF_8).use { properties.load(it) }
-        }
-        val sdkPath = properties.getProperty("flutter.sdk") ?: System.getenv("FLUTTER_ROOT")
-        requireNotNull(sdkPath) { "Flutter SDK not found. Define flutter.sdk in local.properties or FLUTTER_ROOT env variable." }
-        sdkPath
-    }
-
-    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
-
     repositories {
         google()
         mavenCentral()
@@ -20,9 +7,9 @@ pluginManagement {
 }
 
 plugins {
-    id("com.android.application") version "8.1.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.22" apply false // Downgrade a versione stabile per bloccare i crash dei plugin
-    id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false
+    id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply true
+    id("com.android.application") version "8.7.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
 }
 
 include(":app")
