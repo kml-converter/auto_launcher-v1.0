bcc2b43edddac80a0de1dfe55f24c7f63b3e1066
kml-converter <ing.alflab@gmail.com>
2026-07-21 11:20:23 +0200

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -11,12 +11,19 @@ pluginManagement {
             ?: throw GradleException("Flutter SDK non trovato. Imposta FLUTTER_ROOT o flutter.sdk")
     }
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
+    
+    // CORREZIONE: Dice a Gradle dove cercare i plugin Android e Kotlin
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
 }
 
 plugins {
     id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false
-    id("com.android.application") version "7.3.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.7.10" apply false
+    id("com.android.application") version "8.1.0" apply false // Aggiornato per compatibilità
+    id("org.jetbrains.kotlin.android") version "1.8.22" apply false // Aggiornato per compatibilità
 }
 
 include(":app")
