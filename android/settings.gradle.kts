21585bf948a125e031de4ebcac6063e42cc4b2cd
kml-converter <ing.alflab@gmail.com>
2026-07-21 10:46:54 +0200

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,15 +1,11 @@
 pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
+    val flutterSdkPath = settingsDir.parentFile.resolve(".flutter-sdk").absolutePath
+    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
 }
-
 plugins {
-    id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply true
+    id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false
     id("com.android.application") version "8.7.0" apply false
     id("org.jetbrains.kotlin.android") version "2.0.0" apply false
 }
-
 include(":app")
+
