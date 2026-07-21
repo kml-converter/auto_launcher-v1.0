ee0733dd42f30dbb3e30a81d86373420568138e3
kml-converter <ing.alflab@gmail.com>
2026-07-21 10:51:14 +0200

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,11 +1,15 @@
 pluginManagement {
-    val flutterSdkPath = settingsDir.parentFile.resolve(".flutter-sdk").absolutePath
+    val flutterSdkPath = System.getenv("FLUTTER_ROOT")
+        ?: properties.getProperty("flutter.sdk") 
+        ?: throw GradleException("Flutter SDK non trovato. Imposta FLUTTER_ROOT o flutter.sdk")
+
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
 }
+
 plugins {
     id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false
-    id("com.android.application") version "8.7.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
+    id("com.android.application") version "7.3.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.7.10" apply false
 }
-include(":app")
 
+include(":app")
